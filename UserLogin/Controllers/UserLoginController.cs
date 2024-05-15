f8d765c7e33e66d62ec82d1f41c3670202cdfb7c
Prathamesh Gaikwad <prathamesh.gaikwad.itvidpra@gmail.com>
2024-05-15 21:51:46 +0530

    Implemented UserLogin Index Page

---
+++ UserLogin/Controllers/UserLoginController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserLogin.Models;
+
+namespace UserLogin.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        private MenuGDbContext db = new MenuGDbContext();
+
+        // GET: UserLogin
+        public ActionResult Index()
+        {
+            var list = db.UserLogins.ToList();
+            return View(list);
+        }
+
+        // GET: UserLogin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserLogin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserLogin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserLogin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserLogin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserLogin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserLogin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
